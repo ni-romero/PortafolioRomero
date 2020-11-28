e0527ab130a70878c8ceed00981d56e7b405f640
ni-romero <luz.nicole.maria@gmail.com>
2020-11-27 22:13:38 -0300

    get

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded( ({
     extended: false
 })));
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+    app.use(express.static(path.join(__dirname, 'client/build')));
+
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+};
 
 
 app.post("/api/form", (req, res) => { 
